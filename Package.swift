7480e32918c8dd0dae5dec0f963ad5d8dc1991da
future <807749729@qq.com>
2024-08-21 20:43:23 +0800

    aasdf

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "SPMTest",
             type: .static,
-            targets: ["SPMTest","ResourceAAA", "UPFirstFramework"]),
+            targets: ["SPMTest","ResourceAAA","UPFirstFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
